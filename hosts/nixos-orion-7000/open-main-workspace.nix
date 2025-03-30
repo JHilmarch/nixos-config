3ff370e2dee771169f80e30bfbb776eb2b03c003
Jonatan Hilmarch <JHilmarch@users.noreply.github.com>
2025-03-30 13:28:09 +0200

    feat(nixos-orion-7000): open web apps
    In the Main workspace, open MS365 and Proton apps tohether
    with Firefox.
    
    Relates to: #1

--- hosts/nixos-orion-7000/open-main-workspace.nix
+++ hosts/nixos-orion-7000/open-main-workspace.nix
@@ -4,8 +4,12 @@ pkgs.writeShellScript "main-workspace-script" ''
   # Switch to workspace 1 (Main)
   wmctrl -s 0
 
-  # Launch Calendar, Firefox, and System Monitor
+  # Launch Calendar, Firefox, and Thunderbird (TODO)
   ${pkgs.gnome-calendar}/bin/gnome-calendar && sleep 0.3 &
-  ${pkgs.firefox}/bin/firefox --new-window && sleep 0.3 &
-  ${pkgs.gnome-system-monitor}/bin/gnome-system-monitor &
+  ${pkgs.firefox}/bin/firefox -url \
+    "https://planner.cloud.microsoft/webui/mytasks/assignedtome/view/board?tid=8b21d9cf-fc02-4ed2-aae6-3c2fc6ca5c1f" \
+    "https://outlook.office.com/mail/" \
+    "https://teams.microsoft.com/v2/" \
+    "https://mail.proton.me/" \
+    "https://calendar.proton.me/" --new-window &
 ''
