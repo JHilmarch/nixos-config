a898cdfc821b899cce21160126530cac570c463c
Jonatan Hilmarch <JHilmarch@users.noreply.github.com>
2026-01-03 16:46:52 +0100

    feat: update nixos and hm to v25.11

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Jonatan's NixOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
     mcp-nixos.url = "github:utensils/mcp-nixos";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
