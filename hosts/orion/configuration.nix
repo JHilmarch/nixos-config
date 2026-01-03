5edc1d45e38312fb886c925e63f26bf96a4dbe90
Jonatan Hilmarch <JHilmarch@users.noreply.github.com>
2026-01-03 21:35:53 +0100

    feat(orion): use context7 sops wrapper

--- hosts/orion/configuration.nix
+++ hosts/orion/configuration.nix
@@ -17,6 +17,7 @@ in {
   imports = [
     ./modules/sops.nix
     ./modules/docker.nix
+    "${self}/modules/context7/sops-wrapper.nix"
     "${self}/modules/claude/default.nix"
     "${self}/modules/nfs/fileshare.nix"
     "${self}/modules/systemd/no-sleep.nix"
