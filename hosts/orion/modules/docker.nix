e339c895d90ff45ad1ce3d9160202d9a2431f99b
Jonatan Hilmarch <JHilmarch@users.noreply.github.com>
2026-01-04 15:08:33 +0100

    fix(orion): docker service restrictions
    Prevent Docker from starting in GDM session.
    Only allow it in the actual user session.

--- hosts/orion/modules/docker.nix
+++ hosts/orion/modules/docker.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  username,
   ...
 }: {
   virtualisation.docker = {
@@ -10,6 +11,14 @@
     rootless = {
       enable = true;
       setSocketVariable = true;
+    };
+  };
+
+  # Prevent Docker from starting in GDM session
+  # Only allow it in the actual user session
+  systemd.user.services.docker = {
+    unitConfig = {
+      ConditionUser = lib.mkForce username;
     };
   };
 }
