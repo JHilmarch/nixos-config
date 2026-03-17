93db99cbdcb38d5d21e70a493e982f89337eabe8
Jonatan Hilmarch <JHilmarch@users.noreply.github.com>
2026-03-17 02:15:30 +0100

    testa nya packages för github mcp orion

--- packages/github-personal-mcp/default.nix
+++ packages/github-personal-mcp/default.nix
@@ -13,7 +13,7 @@ in
     postBuild = ''
       rm $out/bin/github-mcp-server
       makeWrapper ${github-mcp-server}/bin/github-mcp-server $out/bin/${name} \
-        --run 'GITHUB_PERSONAL_ACCESS_TOKEN="$(cat "''${XDG_RUNTIME_DIR}/secrets/gh_personal_pat" 2>/dev/null || cat /run/secrets/gh_personal_pat)"'
+        --run 'GITHUB_PERSONAL_ACCESS_TOKEN="$(cat "''${XDG_RUNTIME_DIR}/secrets/gh_personal_pat" 2>/dev/null || cat "/run/user/$(id -u)/secrets/gh_personal_pat" 2>/dev/null || cat /run/secrets/gh_personal_pat | xargs)"'
     '';
 
     meta = with lib; {
