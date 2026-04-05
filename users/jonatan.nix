562ab991429a077acc00d1ee400e24ccf51b8585
Jonatan Hilmarch <JHilmarch@users.noreply.github.com>
2026-04-05 06:32:20 +0200

    style(users): format jonatan.nix

--- users/jonatan.nix
+++ users/jonatan.nix
@@ -1,4 +1,8 @@
-{functions, pkgs, ...}: let
+{
+  functions,
+  pkgs,
+  ...
+}: let
   authorizedSSHKeys = functions.ssh.getGithubKeys {
     username = "JHilmarch";
     sha256 = "1zxj95jlhabgbaxvvhlhwvxlr6xn00ldx6yaz3sdga55wbcnsw34";
